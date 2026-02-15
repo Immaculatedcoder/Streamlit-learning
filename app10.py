dee21c62b6b051a587eae43aa238f189fef3e6dd
Adebayo Emmanuel <immaculatedcoder@gmail.com>
2026-02-15 10:40:28 -0500

    Done with Day 10

--- app10.py
+++ app10.py
@@ -1,10 +1,23 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
 
 st.title("🎁 st.selectbox")
 
-option = st.selectbox(
-    'What is your favorite color?',
-    ('Blue', 'Red', 'Green')
+df = pd.DataFrame(
+    np.random.randn(200,3),
+    columns=['a','b','c']
 )
 
-st.write(f'Your favortite color is: {option}')
+selected_column = st.selectbox(
+    "Select a Dataframe column",
+    df.columns
+)
+st.write(selected_column)
+st.line_chart(df, x=None, y=selected_column)
+# option = st.selectbox(
+#     'What is your favorite color?',
+#     ('Blue', 'Red', 'Green')
+# )
+
+# st.write(f'Your favortite color is: {option}')
