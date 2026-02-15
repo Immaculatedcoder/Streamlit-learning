18f4830f632cc0856ffd8e5e79aadb111b197d0d
Adebayo Emmanuel <immaculatedcoder@gmail.com>
2026-02-15 10:50:07 -0500

    Day 11 done

--- app11.py
+++ app11.py
@@ -1,1 +1,14 @@
 import streamlit as st
+
+st.title("st.multiselct")
+
+option = st.multiselect(
+    'What are your favorite colors',
+    ['Green', 'Yellow', 'Red', 'Blue'],
+    ['Yellow', 'Red']
+)
+
+st.write('You selected: ', option)
+
+for color in option:
+    st.write(color)
